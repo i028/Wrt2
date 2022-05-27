18c89c7004e56a2275ceb2999de94ff6cd7e7600
coss <i60888@163.com>
2022-05-27 11:29:19 +0800

    new

--- diy2.sh
+++ diy2.sh
@@ -9,4 +9,9 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/kenzok8/small-package package/small-package
+
+rm -rf ./package/feeds/luci/luci-theme-netgear
+git clone https://github.com/i028/luci-theme-netgear package/luci-theme-netgear
